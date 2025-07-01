4092e295b156201294f0047732e64d5781595125
Noof <202102651@stu.uob.edu.bh>
2025-07-01 16:31:39 +0300

    lines and basics

--- js/app.js
+++ js/app.js
@@ -1,15 +1,25 @@
 
 /*-------------- Constants -------------*/
+const alphabet =['a', 'b', 'c', 'd', 'e', 'f', 'g',
+                 'h', 'i', 'j', 'k', 'l', 'm', 'n', 
+                 'o', 'p', 'q', 'r', 's', 't', 'u',
+                  'v', 'w', 'x', 'y', 'z'];
+
+const fruite =['apple', 'grape', 'melon', 'mango', 'peach', 'dates', 'berry', 'lemon', 'olive', 'guava'];
 
 
 /*---------- Variables (state) ---------*/
 
 
 /*----- Cached Element References  -----*/
+const lines = document.querySelectorAll('.dash');
 
 
 /*-------------- Functions -------------*/
-
+lines.forEach(element => {
+    element.textContent = "_";
+  });
 
 /*----------- Event Listeners ----------*/
 
+  
